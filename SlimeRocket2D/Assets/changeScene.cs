92f59dd76a427659af48ce42c111885c4e4234cb
Ryan Chartier <ryefye@outlook.com>
2016-06-01 02:03:59 -0400

    Added MainMenu and level loading

---
+++ SlimeRocket2D/Assets/changeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+    public int id;
+    public void loadScene(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                //load the game
+                SceneManager.LoadScene(1);
+                Debug.Log("Pressed");
+                break;
+        }
+    }
+}
